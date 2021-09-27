dc1ac1deb89ea4a571cb31c247e67dbf9204750c
yanlai00 <yyang22@berkeley.edu>
2021-09-27 01:55:37 -0700

    rename spt to imitation learning

--- semiparametrictransfer_experiments/modeltraining/widowx/real/toy_kitchen_v0/task_id_conditioned/conf_b8.py
+++
@@ -1,6 +0,0 @@
-from semiparametrictransfer_experiments.modeltraining.widowx.real.toy_kitchen_v0.task_id_conditioned import conf
-configuration = conf.configuration
-data_config = conf.data_config
-model_config = conf.model_config
-
-configuration.batch_size = 8
